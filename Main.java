d36652ea1546ce68bec77113eb6a9bb3bfe570f6
C0r1l <carolygonzalez1458@gmail.com>
2025-10-21 21:46:18 +0000

    hola

--- Main.java
+++ Main.java
@@ -5,5 +5,37 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+
+    // Problem 1
+
+    for (int i = 1; i <= 25; i += 2)
+    {
+
+        System.out.println( i + " ");
+
+
+    }
+
+    // Problem 2
+
+    int counter = 0; 
+
+   for (int i = 17; i <= 75; i++)
+    {
+        if (counter % 10 == 9)
+        {
+
+         System.out.println( i + " " );
+
+        }
+        else 
+        {
+            System.out.println( i + " " );
+
+        }
+
+        counter++;
+    }
+
   }
 }
